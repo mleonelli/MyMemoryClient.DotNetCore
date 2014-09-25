014d138c24ccd27cd6f69da19fe473baef795c6c
Mauro Leonelli <mleonelli@gmail.com>
2014-09-25 15:28:17 +0200

    Adding the first release of the C# Library

---
+++ C#/MyMemoryAPI/MyMemory.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMemoryAPI
+{
+    public class MyMemory
+    {
+        private static string myMemoryBaseUrl = "http://api.mymemory.translated.net";
+
+        public static void Get()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(myMemoryBaseUrl);
+
+            string uri = "/get" +
+                "?" +
+                "q=" + "Hello World!" + "&" +
+                "langpair=" + "en|it";
+
+            HttpResponseMessage response = client.GetAsync(uri).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                MyMemoryGetResponse ajsonObject = JsonConvert.DeserializeObject<MyMemoryGetResponse>(response.Content.ReadAsStringAsync().Result);
+            }
+            else
+            {
+
+            }
+        }
+
+        public static void Set()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(myMemoryBaseUrl);
+
+
+            string uri = "/set" +
+                "?" +
+                "seg=" + "Hello World!" + "&" +
+                "tra=" + "Ciao Mondo!" + "&" +
+                "langpair=" + "en|it";
+
+
+            //form.Add(new StringContent(tra), "key");
+            //form.Add(new StringContent(langpair), "de");
+            HttpResponseMessage response = client.GetAsync(uri).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                MyMemoryGetResponse ajsonObject = JsonConvert.DeserializeObject<MyMemoryGetResponse>(response.Content.ReadAsStringAsync().Result);
+            }
+            else
+            {
+
+            }
+        }
+
+        public static void Keygen(string username, string password)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(myMemoryBaseUrl);
+
+            string uri = "/keygen";
+
+            MultipartFormDataContent form = new MultipartFormDataContent();
+
+            form.Add(new StringContent(username), "user");
+            form.Add(new StringContent(password), "pass");
+
+            HttpResponseMessage response = client.PostAsync(uri, form).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                MyMemoryGetResponse ajsonObject = JsonConvert.DeserializeObject<MyMemoryGetResponse>(response.Content.ReadAsStringAsync().Result);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ C#/MyMemoryAPI/MyMemoryResponse.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyMemoryAPI
+{
+
+    public class MyMemorySetResponse
+    {
+        public string responseData { get; set; }
+        public string responseDetails { get; set; }
+        public int responseStatus { get; set; }
+        public string responderId { get; set; }
+        public List<Match> matches { get; set; }
+    }
+
+    public class MyMemoryGetResponse
+    {
+        public ResponseData responseData { get; set; }
+        public string responseDetails { get; set; }
+        public int responseStatus { get; set; }
+        public string responderId { get; set; }
+        public List<Match> matches { get; set; }
+    }
+
+    public class ResponseData
+    {
+        public string translatedText { get; set; }
+    }
+
+    public class Match
+    {
+        public string id { get; set; }
+        public string segment { get; set; }
+        public string translation { get; set; }
+        public string quality { get; set; }
+        public string reference { get; set; }
+        [JsonProperty(PropertyName = "usage-count")]
+        public int usageCount { get; set; }
+        public string subject { get; set; }
+        [JsonProperty(PropertyName = "created-by")]
+        public string createdBy { get; set; }
+        [JsonProperty(PropertyName = "last-updated-by")]
+        public string lastUpdatedBy { get; set; }
+        [JsonProperty(PropertyName = "create-date")]
+        public DateTime createDate { get; set; }
+        [JsonProperty(PropertyName = "last-update-date")]
+        public DateTime lastUpdateDate { get; set; }
+        public double match { get; set; }
+    }
+
+}
